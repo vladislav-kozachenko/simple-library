3f8189ce2638461b44a31159ab546eabad7addc2
Kozachenko <vladislav.kozachenko@teamdev.com>
2017-12-12 17:57:57 +0200

    Authentication support: added user session.

---
+++ src/main/java/javaclasses/library/impl/UserSession/UserSession.java
@@ -0,0 +1,23 @@
+package javaclasses.library.impl.UserSession;
+
+import javaclasses.library.impl.user.User;
+
+import java.util.Date;
+
+public class UserSession {
+
+    private long id;
+
+    private Date date;
+
+    private String token;
+
+    private User user;
+
+    public UserSession(Date date, String token, User user) {
+        this.date = date;
+        this.token = token;
+        this.user = user;
+    }
+    
+}
