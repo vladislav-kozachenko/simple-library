53528b30cc833e9748a3cf4c5d12faaa4d29fb47
Kozachenko <vladislav.kozachenko@teamdev.com>
2017-12-12 13:16:48 +0200

    Added library implementation.

---
+++ src/main/java/javaclasses/library/impl/LibraryImpl.java
@@ -0,0 +1,33 @@
+package javaclasses.library.impl;
+
+import javaclasses.library.Library;
+import javaclasses.library.impl.book.Book;
+import javaclasses.library.impl.user.User;
+
+
+/**
+ * Represents implementation of library management system.
+ */
+public class LibraryImpl implements Library {
+
+    @Override
+    public void addBook(Book book) {
+
+    }
+
+    @Override
+    public void borrowBook(String securityToken, Book book) {
+
+    }
+
+    @Override
+    public void createUser(User user) {
+
+    }
+
+    @Override
+    public String loginUser() {
+        return null;
+    }
+
+}
