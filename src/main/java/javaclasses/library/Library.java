3cd02ee3bb699f91854c48348a080a24a3b36159
Kozachenko <vladislav.kozachenko@teamdev.com>
2017-12-14 11:18:58 +0200

    Documentation: edited documentation for getting book by id in public API.

--- src/main/java/javaclasses/library/Library.java
+++ src/main/java/javaclasses/library/Library.java
@@ -23,6 +23,13 @@ public interface Library {
      */
     void addBook(String securityToken, BookVO book, long... authorIds) throws IllegalAccessException;
 
+    /**
+     * Finds the book which has inputted id.
+     * @param securityToken unique security token may be used to get user and check if the user is logged in and has
+     *                      required permission.
+     * @param id is the identifier of needed book.
+     * @return the book with inputted id.
+     */
     Book getBookById(String securityToken, long id);
 
     /**
